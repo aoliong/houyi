c42f55e86b901c863e5e1dbb3f44f2d4ef021350
aoliong <lyx234@gmail.com>
2018-07-11 23:46:05 +0800

    增加查找其它景点链接

---
+++ REDAME.md
@@ -0,0 +1,5 @@
+# Houyi 项目
+
+这是一个关于爬虫的项目。
+
+主要使用Scrapy框架来实现。参考[这里](http://scrapy-chs.readthedocs.io/zh_CN/0.24/topics/spiders.html)
--- houyi/spiders/houyi_spider.py
+++ houyi/spiders/houyi_spider.py
@@ -6,21 +6,19 @@ import scrapy
 from houyi.items import HouyiItem
 
 class Spider123(scrapy.Spider):
-    name = "123"
+    name = "ctrip"
     allowed_domains = ['http://vacations.ctrip.com']
     start_urls = [
+            'http://weekend.ctrip.com/around/guangzhou-dest/taocan/?keyword=%E5%B9%BF%E4%B8%9C#ctm_ref=gs-100000792-290801-32-02-G001|04|guangdong',
             'http://vacations.ctrip.com/tour/detail/p19489584r2001.html',
             'http://vacations.ctrip.com/tour/detail/p19494159r2001.html'
             ]
 
-#   def parse(self, response):
-#       filename = response.url.split('/')[-4]
-#       with open(filename, 'wb') as f:
-#           f.write(response.body)
-
     def parse(self, response):
         # http://vacations.ctrip.com/tour/detail/p19489584r2001.html
         # //div[@class="product_feature"]/span/p/span/text()
+        # 获取其它景点的链接
+        # //div[@class="product_m"]/h2/a/@href
         for sel in response.xpath('//div[@class="product_feature"]/span/p/span/text()'):
             item = HouyiItem()
             item['body'] = sel.extract()
