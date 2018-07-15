5db11216203a8e44373b6a309c22794278848c36
aoliong <lyx234@gmail.com>
2018-07-16 00:11:29 +0800

    可以打印出链接，但未获取到正确的链接和内容

--- houyi/spiders/houyi_spider.py
+++ houyi/spiders/houyi_spider.py
@@ -2,27 +2,37 @@
 #coding=utf8
 
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 from houyi.items import HouyiItem
 
-class Spider123(scrapy.Spider):
+class Spider123(CrawlSpider):
     name = "ctrip"
-    allowed_domains = ['http://vacations.ctrip.com']
+    allowed_domains = ['vacations.ctrip.com']
     start_urls = [
             'http://weekend.ctrip.com/around/guangzhou-dest/taocan/?keyword=%E5%B9%BF%E4%B8%9C#ctm_ref=gs-100000792-290801-32-02-G001|04|guangdong',
-            'http://vacations.ctrip.com/tour/detail/p19489584r2001.html',
-            'http://vacations.ctrip.com/tour/detail/p19494159r2001.html'
             ]
 
-    def parse(self, response):
+    rules = (
+            Rule(LinkExtractor(), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
         # http://vacations.ctrip.com/tour/detail/p19489584r2001.html
         # //div[@class="product_feature"]/span/p/span/text()
         # 获取其它景点的链接
         # //div[@class="product_m"]/h2/a/@href
+
+        self.logger.info('****hello')
+        self.logger.info('Hi, this is an item page! %s' % response.url)
+
         for sel in response.xpath('//div[@class="product_feature"]/span/p/span/text()'):
             item = HouyiItem()
             item['body'] = sel.extract()
-            print item['body']
+            self.logger.info('body is: %s' % item['body'])
             yield item
             # print sel.extract()
+
+
 
